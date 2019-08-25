6657804af877ec4cc3406df9f3f9c8873f4bac0e
taosy <342640669@qq.com>
2019-08-25 22:32:23 +0800

    解决一些冲突

--- standard-main/src/test/java/com/vailter/standard/FileCopyTest.java
+++ standard-main/src/test/java/com/vailter/standard/FileCopyTest.java
@@ -20,9 +20,16 @@ public class FileCopyTest {
         File source = new File("D:\\test1.txt");
         File dest = new File("D:\\test2.txt");
         copyFileByStream(source, dest);
+        File dest2 = new File("D:\\test3.txt");
+//        copyFileByStream(source, dest);
+//        copyFileByChannel(source, dest2);
     }
 
     private static void readFile(File dest) throws IOException {
+
+        if (!dest.exists()) {
+            return;
+        }
         FileReader reader = new FileReader(dest);//定义一个fileReader对象，用来初始化BufferedReader
         BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(dest), "UTF-8"));//new一个BufferedReader对象，将文件内容读取到缓存
 
@@ -38,6 +45,17 @@ public class FileCopyTest {
         System.out.println(str);
     }
 
+    /**
+     * 使用了用户态空间的拷贝
+     * <p>
+     * 我们使用输入输出流进行读写时，实际上是进行了多次上下文切换，比如应用读取数据时，先 在内核态将数据从磁盘读取到内核缓存，再切换到用户态将数据从内核缓存读取到用户缓存。
+     * <p>
+     * 对于 Copy 的效率，这个其实与操作系统和配置等情况相关，总体上来说，NIO transferTo/From 的方式可能更快，因为它更能利用现代操作系统底层机制，避免不必要拷贝 和上下文切换。
+     *
+     * @param source
+     * @param dest
+     * @throws IOException
+     */
     public static void copyFileByStream(File source, File dest) throws IOException {
         try (InputStream is = new FileInputStream(source);
              OutputStream os = new FileOutputStream(dest)) {
@@ -73,5 +91,28 @@ public class FileCopyTest {
     private static void copyFileUsingJava7Files(File source, File dest)
             throws IOException {
         Files.copy(source.toPath(), dest.toPath());
+    }
+
+    /**
+     * java.nio 类库提供的 transferTo 或 transferFrom 方法实现。
+     * <p>
+     * 基于 NIO transferTo 的实现方式，在 Linux 和 Unix 上，
+     * 则会使用到零拷贝技术，数据传输 并不需要用户态参与，省去了上下文切换的开销和不必要的内存拷贝，
+     * 进而可能提高应用拷贝性 能。注意，transferTo 不仅仅是可以用在文件拷贝中，
+     * 与其类似的，例如读取磁盘文件，然后 进行 Socket 发送，同样可以享受这种机制带来的性能和扩展性提高。
+     *
+     * @param source
+     * @param dest
+     * @throws IOException
+     */
+    public static void copyFileByChannel(File source, File dest) throws
+            IOException {
+        try (FileChannel sourceChannel = new FileInputStream(source).getChannel();
+             FileChannel targetChannel = new FileOutputStream(dest).getChannel()) {
+            for (long count = sourceChannel.size(); count > 0; ) {
+                long transferred = sourceChannel.transferTo(sourceChannel.position(), count, targetChannel);
+                count -= transferred;
+            }
+        }
     }
 }
