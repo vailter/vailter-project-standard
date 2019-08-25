c442ef92cce775274e8c1e44e00e42f0439f827f
vailter <342640669@qq.com>
2019-08-25 22:22:03 +0800

    添加一些功能性：Spring Boot处理静态资源

---
+++ standard-main/src/main/java/com/vailter/standard/controller/StaticResourceController.java
@@ -0,0 +1,54 @@
+package com.vailter.standard.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * static-path-pattern：访问模式，默认为/**，多个可以逗号分隔
+ * <p>
+ * static-locations：资源目录，多个目录逗号分隔，
+ * 默认资源目录为classpath:/META-INF/resources/,classpath:/resources/,classpath:/static/,classpath:/public/
+ * <p>
+ * Spring boot默认对/**的访问可以直接访问四个目录下的文件：
+ * <p>
+ * classpath:/public/
+ * classpath:/resources/
+ * classpath:/static/
+ * classpath:/META-INFO/resouces/
+ */
+@RestController
+public class StaticResourceController {
+    @RequestMapping("/static/**")
+    public void getHtml(HttpServletRequest request, HttpServletResponse response) {
+        String uri = request.getRequestURI();
+        String[] arr = uri.split("static/");
+
+        String resourceName = "index.html";
+
+        if (arr.length > 1) {
+            resourceName = arr[1];
+        }
+
+        String url = StaticResourceController.class.getResource("/").getPath() + "html/" + resourceName;
+        try {
+            FileReader reader = new FileReader(new File(url));
+            BufferedReader br = new BufferedReader(reader);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            response.getOutputStream().write(sb.toString().getBytes());
+            response.flushBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
